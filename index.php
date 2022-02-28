54a31be48e6f455e7d16ab7b2a511aeda0842e9b
rogeritael <rogeritael.contato@gmail.com>
2022-02-28 19:56:47 -0300

    criei o frontend da tela de login

---
+++ index.php
@@ -0,0 +1,23 @@
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="assets/css/styles.css">
+    <title>Login Controle de gastos</title>
+</head>
+<body>
+
+    <section id="container-login">
+        <a href="index.php"><i id="logo" class="fas fa-piggy-bank"></i></a>
+        <form id="form-login" action="">
+            <input class="input" type="text" name="usuario" placeholder="Digite seu usuario">
+            <input class="input" type="password" name="senha" placeholder="Digite sua senha">
+            <input class="input" type="button" value="Entrar">
+        </form>
+        <p class="cadastro-txt"><a href="">Cadastrar</a></p>
+    </section>
+    
+</body>
+</html>
