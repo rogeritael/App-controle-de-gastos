e86ac82354a3a9f1e68f07c7d689506d236234be
rogeritael <rogeritael.contato@gmail.com>
2022-03-09 17:49:19 -0300

    html e css da parte superior da sidebar

--- index.php
+++ index.php
@@ -3,21 +3,44 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="stylesheet" href="assets/css/styles.css">
-    <title>Login Controle de gastos</title>
+    <link rel="stylesheet" href="css/styles.css">
+    <title>Kanban Board</title>
 </head>
-<body class="body-login">
+<body>
+    <div class="container">
+        <aside class="sidebar">
 
-    <section id="container-login">
-        <a href="index.php"><i id="logo" class="fas fa-piggy-bank"></i></a>
-        <form id="form-login-cadastro" action="">
-            <input class="input" type="text" name="usuario" placeholder="Digite seu usuario">
-            <input class="input" type="password" name="senha" placeholder="Digite sua senha">
-            <input class="input" type="button" value="Entrar">
-        </form>
-        <p class="cadastro-txt"><a href="cadastro.php">Cadastrar</a></p>
-    </section>
-    
+            <!-- sidebar header -->
+            <div class="aside-header">
+                <div class="aside-title-container">
+                    <h2>Projetos</h2>
+                    <a href=""><i class="fas fa-plus"></i></a>
+                </div>
+
+                <div class="new-project-form">
+                    <form action="teste.php">
+                        <input type="text" name="" id="" placeholder="Nome do projeto">
+                        <button type="submit"><i class="fas fa-chevron-right"></i></button>
+                    </form>
+                </div>
+            </div>
+            <!-- /sidebar header -->
+
+
+            <div class="aside-body">
+                
+            </div>
+            <div class="aside-footer">
+
+            </div>
+        </aside>
+        
+
+
+    </div>    
 </body>
 </html>
